08c005160e208be973b7514a074f1d0cac4f74c2
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:46:43 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan13/relational/PBO310117096Latihan13Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan data relational
+ */
+public class PBO310117096Latihan13Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 == bil1));
+    }
+    
+}
